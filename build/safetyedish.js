3880e8261daa2f5a7c2c1074dc615f19ddff6ef9
jwildfire <jwildfire@gmail.com>
2018-03-27 10:35:59 -0700

    create reset button. fix #20

--- build/safetyedish.js
+++ build/safetyedish.js
@@ -92,6 +92,175 @@
         });
     };
 
+    var _typeof =
+        typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol'
+            ? function(obj) {
+                  return typeof obj;
+              }
+            : function(obj) {
+                  return obj &&
+                      typeof Symbol === 'function' &&
+                      obj.constructor === Symbol &&
+                      obj !== Symbol.prototype
+                      ? 'symbol'
+                      : typeof obj;
+              };
+
+    var asyncGenerator = (function() {
+        function AwaitValue(value) {
+            this.value = value;
+        }
+
+        function AsyncGenerator(gen) {
+            var front, back;
+
+            function send(key, arg) {
+                return new Promise(function(resolve, reject) {
+                    var request = {
+                        key: key,
+                        arg: arg,
+                        resolve: resolve,
+                        reject: reject,
+                        next: null
+                    };
+
+                    if (back) {
+                        back = back.next = request;
+                    } else {
+                        front = back = request;
+                        resume(key, arg);
+                    }
+                });
+            }
+
+            function resume(key, arg) {
+                try {
+                    var result = gen[key](arg);
+                    var value = result.value;
+
+                    if (value instanceof AwaitValue) {
+                        Promise.resolve(value.value).then(
+                            function(arg) {
+                                resume('next', arg);
+                            },
+                            function(arg) {
+                                resume('throw', arg);
+                            }
+                        );
+                    } else {
+                        settle(result.done ? 'return' : 'normal', result.value);
+                    }
+                } catch (err) {
+                    settle('throw', err);
+                }
+            }
+
+            function settle(type, value) {
+                switch (type) {
+                    case 'return':
+                        front.resolve({
+                            value: value,
+                            done: true
+                        });
+                        break;
+
+                    case 'throw':
+                        front.reject(value);
+                        break;
+
+                    default:
+                        front.resolve({
+                            value: value,
+                            done: false
+                        });
+                        break;
+                }
+
+                front = front.next;
+
+                if (front) {
+                    resume(front.key, front.arg);
+                } else {
+                    back = null;
+                }
+            }
+
+            this._invoke = send;
+
+            if (typeof gen.return !== 'function') {
+                this.return = undefined;
+            }
+        }
+
+        if (typeof Symbol === 'function' && Symbol.asyncIterator) {
+            AsyncGenerator.prototype[Symbol.asyncIterator] = function() {
+                return this;
+            };
+        }
+
+        AsyncGenerator.prototype.next = function(arg) {
+            return this._invoke('next', arg);
+        };
+
+        AsyncGenerator.prototype.throw = function(arg) {
+            return this._invoke('throw', arg);
+        };
+
+        AsyncGenerator.prototype.return = function(arg) {
+            return this._invoke('return', arg);
+        };
+
+        return {
+            wrap: function(fn) {
+                return function() {
+                    return new AsyncGenerator(fn.apply(this, arguments));
+                };
+            },
+            await: function(value) {
+                return new AwaitValue(value);
+            }
+        };
+    })();
+
+    /*------------------------------------------------------------------------------------------------\
+  Clone a variable (http://stackoverflow.com/a/728694).
+\------------------------------------------------------------------------------------------------*/
+
+    function clone(obj) {
+        var copy;
+
+        //Handle the 3 simple types, and null or undefined
+        if (null == obj || 'object' != (typeof obj === 'undefined' ? 'undefined' : _typeof(obj)))
+            return obj;
+
+        //Handle Date
+        if (obj instanceof Date) {
+            copy = new Date();
+            copy.setTime(obj.getTime());
+            return copy;
+        }
+
+        //Handle Array
+        if (obj instanceof Array) {
+            copy = [];
+            for (var i = 0, len = obj.length; i < len; i++) {
+                copy[i] = clone(obj[i]);
+            }
+            return copy;
+        }
+
+        //Handle Object
+        if (obj instanceof Object) {
+            copy = {};
+            for (var attr in obj) {
+                if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+            }
+            return copy;
+        }
+
+        throw new Error("Unable to copy obj! Its type isn't supported.");
+    }
+
     var defaultSettings = {
         //Default template settings
         value_col: 'STRESN',
@@ -540,11 +709,38 @@
         this.measureTable.wrap.selectAll('*').style('display', 'none');
     }
 
+    function initResetButton() {
+        var chart = this;
+
+        this.controls.reset = {};
+        var reset = this.controls.reset;
+        reset.wrap = this.controls.wrap.append('div').attr('class', 'control-group');
+        reset.label = reset.wrap
+            .append('span')
+            .attr('class', 'wc-control-label')
+            .text('Reset Chart');
+        reset.button = reset.wrap
+            .append('button')
+            .text('Reset')
+            .on('click', function() {
+                var initial_container = chart.element;
+                var initial_settings = chart.initial_settings;
+                var initial_data = chart.initial_data;
+
+                chart.destroy();
+                chart = null;
+
+                var newChart = safetyedish(initial_container, initial_settings);
+                newChart.init(initial_data);
+            });
+    }
+
     function onLayout() {
         initQuadrants.call(this);
         initRugs.call(this);
         initVisitPath.call(this);
         initMeasureTable.call(this);
+        initResetButton.call(this);
     }
 
     //Converts a one record per measure data object to a one record per participant objects
@@ -1299,8 +1495,10 @@
         toggleLegend.call(this);
     }
 
-    function safetyedish(element, settings) {
-        var mergedSettings = Object.assign({}, defaultSettings, settings),
+    function safetyedish$1(element, settings) {
+        var initial_settings = clone(settings),
+            defaultSettings_clone = clone(defaultSettings),
+            mergedSettings = Object.assign({}, defaultSettings_clone, settings),
             syncedSettings = syncSettings(mergedSettings),
             syncedControlInputs = syncControlInputs(syncedSettings),
             controls = webcharts.createControls(element, {
@@ -1308,7 +1506,9 @@
                 inputs: syncedControlInputs
             }),
             chart = webcharts.createChart(element, syncedSettings, controls);
+
         chart.element = element;
+        chart.initial_settings = initial_settings;
 
         //Define callbacks.
         chart.on('init', onInit);
@@ -1321,5 +1521,5 @@
         return chart;
     }
 
-    return safetyedish;
+    return safetyedish$1;
 });
--- src/callbacks/onLayout.js
+++ src/callbacks/onLayout.js
@@ -2,10 +2,12 @@ import { initQuadrants } from './onLayout/initQuadrants';
 import { initRugs } from './onLayout/initRugs';
 import { initVisitPath } from './onLayout/initVisitPath';
 import { initMeasureTable } from './onLayout/initMeasureTable';
+import { initResetButton } from './onLayout/initResetButton';
 
 export default function onLayout() {
     initQuadrants.call(this);
     initRugs.call(this);
     initVisitPath.call(this);
     initMeasureTable.call(this);
+    initResetButton.call(this);
 }
