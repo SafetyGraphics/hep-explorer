f1d29cf666a2cb2cf6d2657820966ca7b2d65331
jwildfire <jwildfire@gmail.com>
2018-05-15 10:39:31 -0700

    add test page. close #52

---
+++ test/index.js
@@ -0,0 +1,31 @@
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+      //Load local build if in local environment.
+      if (window.origin !== 'https://rhoinc.github.io') {
+          var head = document.getElementsByTagName('head')[0];
+
+        //...load local build.
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = '../build/safetyedish.js';
+          head.appendChild(script);
+      }
+
+      d3.csv(
+          'https://rawgit.com/RhoInc/viz-library/master//data/safetyData-queries/ADBDS.csv',
+          function(error,data) {
+              if (error)
+                  console.log(error);
+
+                  let settings = {
+                    display:"relative",
+                    group_cols:["ARM","RACE"]
+                  }
+
+                  let chart = safetyedish('#container', settings);
+                  chart.init(data);
+
+          }
+      );
+    }
+}
