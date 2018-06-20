f488142b592f05f7380d18f5403b6aec1d9d06ef
samussiah <samussiah@gmail.com>
2018-06-20 14:08:30 -0400

    fix missing value imputation

--- src/callbacks/onPreprocess/imputeData.js
+++ src/callbacks/onPreprocess/imputeData.js
@@ -4,7 +4,10 @@ export function imputeData() {
     var chart = this,
         config = this.config;
 
-    this.imputed_data = this.initial_data;
+    //Remove missing values via the ultimate number regular expression.
+    this.imputed_data = this.initial_data.filter(d =>
+        /^-?(\d*\.?\d+|\d+\.?\d*)(E-?\d+)?$/.test(d[this.config.value_col])
+    );
     this.imputed_data.forEach(function(d) {
         d.impute_flag = false;
     });
