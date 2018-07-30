3ce0eef991848ff5f61b5da13f09a0417c9741e2
samussiah <samussiah@gmail.com>
2018-07-30 10:04:48 -0400

    add r-ratio property to participant object

--- build/safetyedish.js
+++ build/safetyedish.js
@@ -960,9 +960,16 @@ function calculateRatios(d, participant_obj) {
         _this.config.measure_details.filter(function (di) {
             return di.measure !== d.measure;
         }).forEach(function (di) {
-            participant_obj[d.label + "_relative_uln/" + di.label + "_relative_uln"] = participant_obj[d.label + "_relative_uln"] / participant_obj[di.label + "_relative_uln"];
+            participant_obj[d.label + '_relative_uln/' + di.label + '_relative_uln'] = participant_obj[d.label + '_relative_uln'] / participant_obj[di.label + '_relative_uln'];
         });
     });
+
+    //R-ratio should be the ratio of ALT to ALP, i.e. the x-axis to the z-axis.
+    participant_obj.rRatio = participant_obj[this.config.measure_details.find(function (measure_detail) {
+        return measure_detail.axis === 'x';
+    }).label + '_relative_uln/' + this.config.measure_details.find(function (measure_detail) {
+        return measure_detail.axis === 'z';
+    }).label + '_relative_uln'];
 }
 
 function flattenData() {
--- src/callbacks/onPreprocess/flattenData/calculateRatios.js
+++ src/callbacks/onPreprocess/flattenData/calculateRatios.js
@@ -4,4 +4,15 @@ export default function calculateRatios(d, participant_obj) {
             participant_obj[`${d.label}_relative_uln/${di.label}_relative_uln`] = participant_obj[`${d.label}_relative_uln`] / participant_obj[`${di.label}_relative_uln`];
         });
     });
+
+    //R-ratio should be the ratio of ALT to ALP, i.e. the x-axis to the z-axis.
+    participant_obj.rRatio = participant_obj[
+        `${this.config.measure_details
+            .find(measure_detail => measure_detail.axis === 'x')
+            .label
+        }_relative_uln/${this.config.measure_details
+            .find(measure_detail => measure_detail.axis === 'z' )
+            .label
+        }_relative_uln`
+    ];
 }
