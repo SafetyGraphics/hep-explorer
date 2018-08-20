2a47a7cc814a785979c5afeb3e5b8ea31a775e6b
jwildfire <jwildfire@gmail.com>
2018-08-19 21:36:41 -0700

    more cleanData() to onInit. fix #124

--- src/callbacks/onInit.js
+++ src/callbacks/onInit.js
@@ -1,9 +1,11 @@
 import checkMeasureDetails from './onInit/checkMeasureDetails';
 import iterateOverData from './onInit/iterateOverData';
 import addRRatioFilter from './onInit/addRRatioFilter';
+import { cleanData } from './onInit/cleanData';
 
 export default function onInit() {
     checkMeasureDetails.call(this);
     iterateOverData.call(this);
     addRRatioFilter.call(this);
+    cleanData.call(this); //clean visit-level data - imputation and variable derivations
 }
