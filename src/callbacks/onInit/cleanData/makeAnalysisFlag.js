bbb56a151dc7943f512eb0965c825a49a590c9a7
jwildfire <jwildfire@rhoworld.com>
2018-10-16 20:18:44 -0700

    & -> &&

--- src/callbacks/onInit/cleanData/makeAnalysisFlag.js
+++ src/callbacks/onInit/cleanData/makeAnalysisFlag.js
@@ -3,7 +3,7 @@ export default function makeAnalysisFlag() {
     var config = this.config;
     this.imputed_data = this.imputed_data.map(function(d) {
         const hasAnalysisSetting =
-            (config.analysisFlag.value_col != null) & (config.analysisFlag.values.length > 0);
+            (config.analysisFlag.value_col != null) && (config.analysisFlag.values.length > 0);
         d.analysisFlag = hasAnalysisSetting
             ? config.analysisFlag.values.indexOf(d[config.analysisFlag.value_col]) > -1
             : true;
