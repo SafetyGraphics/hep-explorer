039f5d69d73527c43da23a07d792036248ae1a70
Preston Burns <Preston_Burns@rhoworld.com>
2019-04-23 12:37:59 -0400

    move details string fix

--- src/configuration/syncSettings.js
+++ src/configuration/syncSettings.js
@@ -65,6 +65,11 @@ export default function syncSettings(settings) {
             });
     }
 
+    //parse details to array if needed
+    if (!(settings.details instanceof Array)) {
+        settings.details = typeof settings.details == 'string' ? [settings.details] : [];
+    }
+
     //If [settings.details] is not specified:
     if (!settings.details) settings.details = defaultDetails;
     else {
@@ -111,11 +116,6 @@ export default function syncSettings(settings) {
 
     if (!(settings.y_options instanceof Array)) {
         settings.y_options = typeof settings.y_options == 'string' ? [settings.y_options] : [];
-    }
-
-    //parse details to array if needed
-    if (!(settings.details instanceof Array)) {
-        settings.details = typeof settings.details == 'string' ? [settings.details] : [];
     }
 
     // track initial Cutpoint (lets us detect when cutpoint should change)
