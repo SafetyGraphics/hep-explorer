d574b83e8ba98d695549ec4250951bfa5654051e
jwildfire <jwildfire@gmail.com>
2019-06-05 10:07:21 -0700

    coerce participant level meta data to character. fix #249

--- src/callbacks/onPreprocess/flattenData/addParticipantLevelMetadata.js
+++ src/callbacks/onPreprocess/flattenData/addParticipantLevelMetadata.js
@@ -20,6 +20,6 @@ export default function addParticipantLevelMetadata(d, participant_obj) {
     }
 
     varList.forEach(function(v) {
-        participant_obj[v] = d[0][v];
+        participant_obj[v] = '' + d[0][v];
     });
 }
