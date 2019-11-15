dfb8faf5fc4c521047fcc27b906c199695542b40
jwildfire <jwildfire@gmail.com>
2019-11-15 13:03:08 -0300

    add palt example

---
+++ test-page/example5/index.js
@@ -0,0 +1,45 @@
+const settings = {
+  calculate_palt: true,
+  paltFlag: {
+    value_col: "PALT",
+    values: ["1"]
+  },
+  max_width: 600,
+  value_col: "AVAL",
+  measure_col: "PARAM",
+  visit_col: "VISIT",
+  visitn_col: "VISITNUM",
+  studyday_col: "ADY",
+  normal_col_low: "A1LO",
+  normal_col_high: "A1HI",
+  id_col: "USUBJID",
+  group_cols: ["TRTA", "RACE", "AGEGR1"],
+  r_ratio_filter: true,
+  filters: [
+    {
+      value_col: "TRTA",
+      label: "Treatment"
+    },
+    {
+      value_col: "ALT_ELEVATED",
+      label: "ALT Elevated?"
+    }
+  ],
+  measure_values: {
+    ALT: "Alanine Aminotransferase (U/L)",
+    AST: "Aspartate Aminotransferase (U/L)",
+    TB: "Bilirubin (umol/L)",
+    ALP: "Alkaline Phosphatase (U/L)"
+  },
+  baseline: {
+    value_col: "AVISIT",
+    values: ["Baseline"]
+  }
+};
+const chart = hepexplorer("#container", settings);
+d3.csv("palt_adlbc.csv", function(data) {
+  data.forEach(function(d) {
+    d.EPOCH = +d.VISITNUM > 1 ? "Analysis" : "Baseline";
+  });
+  chart.init(data);
+});
